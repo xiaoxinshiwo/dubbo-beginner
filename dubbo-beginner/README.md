ece7ec3c182c8554b938f5ef241665e2eee9257b
zhangyongxin <fyxiaoxin0429@sina.com>
2018-06-03 15:38:41 +0800

    删除重复代码

--- dubbo-beginner/README.md
+++
@@ -1,3 +0,0 @@
-#dubbo-beginner
-1. 使用dubbo-spring-boot-starter
-2. 常规项目既是服务提供方又是服务消费方
--- dubbo-beginner/facade/src/main/java/com/xiaoxin/facade/HelloFacade.java
+++
@@ -1,9 +0,0 @@
-package com.xiaoxin.facade;
-
-/**
- * @Auther zhangyongxin
- * @date 2018/6/3 下午1:26
- */
-public interface HelloFacade {
-    String hello(String name);
-}
